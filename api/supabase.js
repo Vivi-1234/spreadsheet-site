77c38324168569295f26f235d802f360221592c8
Vivi-1234 <vivisesame@gmail.com>
2025-06-27 18:00:22 +0800

    Update supabase.js

--- api/supabase.js
+++ api/supabase.js
@@ -1,9 +1,9 @@
 const { createClient } = require('@supabase/supabase-js');
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
+const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY;
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY);
 
 module.exports = async (req, res) => {
   const { table, method, data, id, query } = req.body;
